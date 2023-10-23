1b635ae7a705a5f498aef61057cc2e6fa017f307
redd <redd@redds.be>
2023-10-23 15:03:52 +0200

    Added 'inputdbl()', 'inputfloat()' and 'inputlong()' for double, float and long inputs respectively.

--- rinputlib.h
+++ rinputlib.h
@@ -168,4 +168,268 @@ int inputint(char * askStr, int intSize, char * lengthError, int acceptNull, cha
     return nbr;
 }
 
+double inputdbl(char * askStr, int dblSize, char * lengthError, int acceptNull, char * nullError) {
+    // Get a double input
+    static char str[1024];
+    double nbr;
+    int isInputValid;
+
+    if (strcmp(askStr, "") == 0) {
+        askStr = ">>> ";
+    }
+
+    if (dblSize == 0 || dblSize > 10) {
+        dblSize = 10;
+    }
+
+    if (strcmp(lengthError, "") == 0) {
+        lengthError = "La longueur de l'entrée est invalide.";
+    }
+
+    if (strcmp(nullError, "") == 0) {
+        nullError = "L'entrée est invalide.'";
+    }
+
+    if (acceptNull == 0) {
+        do {
+            printf("%s", askStr);
+            if (!fgets(str, 1024, stdin)) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("Une erreur inattendue est survenue. Sortie...");
+                exit(EXIT_FAILURE);
+            }
+
+            cleanstr(str);
+
+            char *endPtr;
+
+            errno = 0;
+            nbr = strtod(str, &endPtr);
+            if (errno == ERANGE) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("\033[0m%s\n", lengthError);
+                isInputValid = 0;
+            } else if (strlen(str) > dblSize) {
+                printf("%s\n", lengthError);
+                isInputValid = 0;
+            } else if (*endPtr && *endPtr != '\n') {
+                isInputValid = 0;
+            } else {
+                isInputValid = 1;
+            }
+        } while (!isInputValid);
+    } else if (acceptNull == 1) {
+        do {
+            printf("%s", askStr);
+            if (!fgets(str, 1024, stdin)) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("Une erreur inattendue est survenue. Sortie...");
+                exit(EXIT_FAILURE);
+            }
+
+            cleanstr(str);
+
+            char *endPtr;
+
+            errno = 0;
+            nbr = strtod(str, &endPtr);
+            if (errno == ERANGE) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("\033[0m%s\n", lengthError);
+                isInputValid = 0;
+            } else if (strlen(str) > dblSize) {
+                printf("%s\n", lengthError);
+                isInputValid = 0;
+            } else if (endPtr == str) {
+                printf("%s\n", nullError);
+                isInputValid = 0;
+            } else if (*endPtr && *endPtr != '\n') {
+                isInputValid = 0;
+            } else {
+                isInputValid = 1;
+            }
+        } while (!isInputValid);
+    } else {
+        nbr = 0.0;
+    }
+
+    return nbr;
+}
+
+float inputfloat(char * askStr, int dblSize, char * lengthError, int acceptNull, char * nullError) {
+    // Get a float input
+    static char str[1024];
+    float nbr;
+    int isInputValid;
+
+    if (strcmp(askStr, "") == 0) {
+        askStr = ">>> ";
+    }
+
+    if (dblSize == 0 || dblSize > 10) {
+        dblSize = 10;
+    }
+
+    if (strcmp(lengthError, "") == 0) {
+        lengthError = "La longueur de l'entrée est invalide.";
+    }
+
+    if (strcmp(nullError, "") == 0) {
+        nullError = "L'entrée est invalide.'";
+    }
+
+    if (acceptNull == 0) {
+        do {
+            printf("%s", askStr);
+            if (!fgets(str, 1024, stdin)) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("Une erreur inattendue est survenue. Sortie...");
+                exit(EXIT_FAILURE);
+            }
+
+            cleanstr(str);
+
+            char *endPtr;
+
+            errno = 0;
+            nbr = (float)strtod(str, &endPtr);
+            if (errno == ERANGE) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("\033[0m%s\n", lengthError);
+                isInputValid = 0;
+            } else if (strlen(str) > dblSize) {
+                printf("%s\n", lengthError);
+                isInputValid = 0;
+            } else if (*endPtr && *endPtr != '\n') {
+                isInputValid = 0;
+            } else {
+                isInputValid = 1;
+            }
+        } while (!isInputValid);
+    } else if (acceptNull == 1) {
+        do {
+            printf("%s", askStr);
+            if (!fgets(str, 1024, stdin)) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("Une erreur inattendue est survenue. Sortie...");
+                exit(EXIT_FAILURE);
+            }
+
+            cleanstr(str);
+
+            char *endPtr;
+
+            errno = 0;
+            nbr = (float)strtod(str, &endPtr);
+            if (errno == ERANGE) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("\033[0m%s\n", lengthError);
+                isInputValid = 0;
+            } else if (strlen(str) > dblSize) {
+                printf("%s\n", lengthError);
+                isInputValid = 0;
+            } else if (endPtr == str) {
+                printf("%s\n", nullError);
+                isInputValid = 0;
+            } else if (*endPtr && *endPtr != '\n') {
+                isInputValid = 0;
+            } else {
+                isInputValid = 1;
+            }
+        } while (!isInputValid);
+    } else {
+        nbr = 0;
+    }
+
+    return nbr;
+}
+
+long inputlong(char * askStr, int intSize, char * lengthError, int acceptNull, char * nullError) {
+    // Get a long input
+    static char str[1024];
+    long nbr;
+    int isInputValid;
+
+    if (strcmp(askStr, "") == 0) {
+        askStr = ">>> ";
+    }
+
+    if (intSize == 0 || intSize > 10) {
+        intSize = 10;
+    }
+
+    if (strcmp(lengthError, "") == 0) {
+        lengthError = "La longueur de l'entrée est invalide.";
+    }
+
+    if (strcmp(nullError, "") == 0) {
+        nullError = "L'entrée est invalide.'";
+    }
+
+    if (acceptNull == 0) {
+        do {
+            printf("%s", askStr);
+            if (!fgets(str, 1024, stdin)) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("Une erreur inattendue est survenue. Sortie...");
+                exit(EXIT_FAILURE);
+            }
+
+            cleanstr(str);
+
+            char *endPtr;
+
+            errno = 0;
+            nbr = strtol(str, &endPtr, 10);
+            if (errno == ERANGE) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("\033[0m%s\n", lengthError);
+                isInputValid = 0;
+            } else if (strlen(str) > intSize) {
+                printf("%s\n", lengthError);
+                isInputValid = 0;
+            } else if (*endPtr && *endPtr != '\n') {
+                isInputValid = 0;
+            } else {
+                isInputValid = 1;
+            }
+        } while (!isInputValid);
+    } else if (acceptNull == 1) {
+        do {
+            printf("%s", askStr);
+            if (!fgets(str, 1024, stdin)) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("Une erreur inattendue est survenue. Sortie...");
+                exit(EXIT_FAILURE);
+            }
+
+            cleanstr(str);
+
+            char *endPtr;
+
+            errno = 0;
+            nbr = strtol(str, &endPtr, 10);
+            if (errno == ERANGE) {
+                printf("\033[1;31mErreur: %s\n", strerror(errno));
+                printf("\033[0m%s\n", lengthError);
+                isInputValid = 0;
+            } else if (strlen(str) > intSize) {
+                printf("%s\n", lengthError);
+                isInputValid = 0;
+            } else if (endPtr == str) {
+                printf("%s\n", nullError);
+                isInputValid = 0;
+            } else if (*endPtr && *endPtr != '\n') {
+                isInputValid = 0;
+            } else {
+                isInputValid = 1;
+            }
+        } while (!isInputValid);
+    } else {
+        nbr = 0;
+    }
+
+    return nbr;
+}
+
 #endif // RINPUTLIB_H
