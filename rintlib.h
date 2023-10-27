daa66d8bd3dd3d6474c7ae838a4613734312ecd7
redd <redd@redds.be>
2023-10-27 21:38:10 +0200

    added a header for int manipulation

---
+++ rintlib.h
@@ -0,0 +1,44 @@
+/*
+A C header that handles some int manipulation.
+Copyright (C) 2023 redd
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#ifndef RINTLIB_H
+#define RINTLIB_H
+
+void intobinary(unsigned int nbr, unsigned int length) {
+    unsigned int i;
+    length = length-1;
+
+    if (length > 30) {
+        length = 30;
+    }
+
+    for (i = 1 << length; i > 0; i = i / 2)
+        (nbr & i) ? printf("1") : printf("0");
+}
+
+#endif // RINTLIB_H
+
+/*
+               _     _
+              | |   | |
+  _ __ ___  __| | __| |
+ | '__/ _ \/ _` |/ _` |
+ | | |  __/ (_| | (_| |
+ |_|  \___|\__,_|\__,_|
+*/
